5db1246e0f34407cbbdf3272524400723420a402
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-02 00:57:26 +0530

    Implement logout

--- back-end/src/main/java/lk/ijse/dep13/auth/api/UserHttpController.java
+++ back-end/src/main/java/lk/ijse/dep13/auth/api/UserHttpController.java
@@ -1,9 +1,12 @@
 package lk.ijse.dep13.auth.api;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lk.ijse.dep13.auth.to.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.*;
@@ -15,7 +18,7 @@ public class UserHttpController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/login", consumes = "application/json")
-    public String login(@RequestBody User user){
+    public String login(@RequestBody User user, HttpServletRequest request){
         try(Connection connection = DriverManager
                 .getConnection("jdbc:postgresql://localhost:5432/dep13_auth_app",
                         "postgres", "psql");
@@ -23,6 +26,7 @@ public class UserHttpController {
             ResultSet rst = stm.executeQuery("SELECT * FROM \"user\" WHERE username='%s' AND password='%s'"
                     .formatted(user.username(), user.password()));
             if (rst.next()){
+                request.getSession();
                 return "logged in";
             }else{
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
@@ -32,8 +36,10 @@ public class UserHttpController {
         }
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/logout")
-    public void logout(){
-
+    public void logout(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        if (session != null) session.invalidate();
     }
 }
