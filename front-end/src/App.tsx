166b3c77d5e87efeca46da1beb7def2d647610b5
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-02 01:27:04 +0530

    Add front-end app

---
+++ front-end/src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+import {createBrowserRouter, redirect, RouteObject, RouterProvider} from "react-router";
+import Login from "./view/login/Login.tsx";
+
+type Routes = RouteObject[];
+const routes: Routes = [
+    {
+        index: true,
+        loader: async ()=>{
+            const response =
+                await fetch('http://localhost:8080/app/clients');
+            if (response.status === 200){
+                //
+            }else{
+                return redirect('/login');
+            }
+        },
+        element: <h1>Main</h1>
+    },
+    {
+        path: 'login',
+        Component: Login
+    }
+];
+const router = createBrowserRouter(routes);
+
+function App() {
+
+  return <RouterProvider router={router}/>
+}
+
+export default App
