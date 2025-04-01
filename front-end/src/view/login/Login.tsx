e69d1420841094b48d0e4488c6fc5d938db756dc
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-02 01:35:05 +0530

    Design the UI of the login

--- front-end/src/view/login/Login.tsx
+++ front-end/src/view/login/Login.tsx
@@ -2,7 +2,31 @@ import './Login.css';
 
 function Login() {
     return (
-        <div>Login</div>
+        <main className='d-flex justify-content-center
+         align-items-center vh-100'>
+            <div className='border rounded p-3 text-center'>
+                <h1>
+                    <i className='bi bi-lock-fill'></i>
+                    Login
+                </h1>
+                <p>Enter your login credentials to continue</p>
+                <div>
+                    <label className='mb-2' htmlFor="txt-username">
+                        Username</label>
+                    <input className='form-control text-center'
+                    placeholder='Enter your username here'
+                           id='txt-username' type="text"/>
+                </div>
+                <div className='mt-2'>
+                    <label className='mb-2' htmlFor="txt-password">
+                        Password</label>
+                    <input className='form-control text-center'
+                           placeholder='Enter your password here'
+                           id='txt-password' type="text"/>
+                </div>
+                <button className='btn btn-primary mt-3'>Log in</button>
+            </div>
+        </main>
     );
 }
 
